532a8f6622b56739c8ca3c342f6a727cd05e4756
Gabriel Lima <156330024+gabrielsjlima@users.noreply.github.com>
2024-04-25 17:01:32 +0000

    atualização

--- projetos-pessoais/faq/aquivo.js
+++ projetos-pessoais/faq/aquivo.js
@@ -1,5 +1,16 @@
-const imagem = document.querySelector(".imagem");
+const btn = document.querySelector(".btn")
+const imagem = document.querySelector(".imagem")
+const respostas = document.querySelector(".respostas")
+const container = document.getElementById("container")
 
-function trocarImagem(){
-    imagem.setAttribute("src", "assets/icon-minus.jpg")
-}
+btn.addEventListener("click", function(){
+    
+    if(imagem.getAttribute("src") == "assets/icon-plus.jpg"){
+        imagem.setAttribute("src", "assets/icon-minus.jpg")
+    } else if(imagem.getAttribute("src") == "assets/icon-minus.jpg"){
+                imagem.setAttribute("src", "assets/icon-plus.jpg")
+    }
+
+    respostas.classList.toggle("mostrar")
+    container.classList.toggle("mod-container")
+})
